8efed0e933c8bd85e526f68fff2462e31fdf7cab
hojuly <hojune.choi.me@gmail.com>
2021-11-04 13:19:59 +0900

    custom tableview cell

--- BountyList/BountyList/BountyViewController.swift
+++ BountyList/BountyList/BountyViewController.swift
@@ -9,24 +9,52 @@ import UIKit
 
 
 class BountyViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-        
+
+    let nameList = ["brook", "chopper", "franky", "luffy", "nami", "robin", "sanji", "zoro"]
+    let bountyList = [3300000, 50, 4400000, 30000000, 16000000, 8000000, 77000000, 120000000]
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    // UITableViewDataSource
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return bountyList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        
-        return cell
+        // guard 구문
+        //        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//        let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+//        cell.imgView.image = img
+//        cell.nameLabel.text = nameList[indexPath.row]
+//        cell.bountryLabel.text = "\(bountyList[indexPath.row])"
+//        return cell
+        // if 구문
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell {
+            let img = UIImage(named: "\(nameList[indexPath.row]).jpg")
+            cell.imgView.image = img
+            cell.nameLabel.text = nameList[indexPath.row]
+            cell.bountryLabel.text = "\(bountyList[indexPath.row])"
+            return cell
+        } else {
+            return UITableViewCell()
+        }
+    
     }
     
     // UITableViewDelegate
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("--> \(indexPath.row)")
     }
+}
+
+
+// Custom Cell
+class ListCell: UITableViewCell {
+    @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var bountryLabel: UILabel!
 }
